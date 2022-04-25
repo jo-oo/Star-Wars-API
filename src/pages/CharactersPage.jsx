ee06f9a67ffb042a77e0cc51dc2b8f9237e43b70
jo-oo <johannakoreo@gmail.com>
2022-04-25 18:09:09 +0200

    ouputs all films titles

--- src/pages/CharactersPage.jsx
+++ src/pages/CharactersPage.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
+/*
+//import from React:
+import { useEffect, useState } from 'react'
+//import { Link } from 'react-router-dom'
+//import components, pages & services:
+import StarWarsAPI from '../services/StarWarsAPI'
+import { Card, ListGroup, Row, Col } from 'react-bootstrap'
 
-function CharactersPage() {
-  return (
-    <div>
-      <h1>Characters</h1>
-    </div>
-  )
+//import SearchBar from '../../components/SearchBar'
+//import { useSearchParams } from 'react-router-dom'
+
+*/
+
+
+//detta är en App! Kunde också skrivit det här i App.jsx
+
+//Vi behöver ett sätt att hålla våra todfos när vi hämtar dom.
+/*
+const CharactersPage = () => {
+  const [characters, setCharacters] = useState("") //sätter listan till tom från början
+
+	
+	const getCharacters = async () => {
+		// Get characters from api
+		const data = await StarWarsAPI.getCharacters()
+
+		// update characters state
+		setCharacters(data) 
+		console.log(data.results);
+	}
+
+	// Get characters from api when component is first mounted
+	useEffect(() => {
+		getCharacters()
+	}, [])
+
+*/
+/*
+	//Here is what we ouput on our page
+	return (
+		<> {/*ersätter vår tidigare container med ett Fragment här */
+			/*
+			<h1>Characters</h1>
+		
+
+			{characters && characters.results.map((characters) => ( //mappar över characters-array. finns det charactaers så skriver den ut följande
+				<Card className="card">
+					<Card.Header>{characters.name}</Card.Header> {/*outprints characters name 
+
+				</Card>
+			))}
+	
+		</> 
+	)
 }
 
 export default CharactersPage
+
+*/
--- src/pages/FilmsPage.jsx
+++ src/pages/FilmsPage.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+//import { Link } from 'react-router-dom'
+import StarWarsAPI from '../services/StarWarsAPI'
+import { Card, ListGroup, Row, Col } from 'react-bootstrap'
+
 
 const FilmsPage = () => {
-  return (
-    <div>
-      <h1>Films</h1>
-    </div>
-  )
+  const [films, setFilms] = useState("") //sätter listan till tom från början
+
+	
+	const getFilms = async () => {
+		// Get films from api
+		const data = await StarWarsAPI.getFilms()
+
+		// update characters state
+		setFilms(data) 
+		console.log(data.results);
+	}
+
+	// Get characters from api when component is first mounted
+	useEffect(() => {
+		getFilms()
+	}, [])
+
+
+	//Here is what we ouput on our page
+	return (
+		<> 
+			<h1>Films</h1>
+		
+			{films && films.results.map((films) => ( //mappar över films-array. finns det charactaers så skriver den ut följande
+				<Card className="card">
+					<Card.Header>{films.title}</Card.Header> {/*outprints characters name */}
+
+				</Card>
+			))}
+	
+		</> 
+	)
 }
 
 export default FilmsPage
