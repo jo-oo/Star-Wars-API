b798bcdc7de446acbe2e2ce7929c9a4b29256310
jo-oo <johannakoreo@gmail.com>
2022-04-25 17:44:55 +0200

    outcommented unused routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import Container from 'react-bootstrap/Container'
 import { Routes, Route } from 'react-router-dom' //importerar komponenterna routes(som omsliuter alla våra route, som en förälderkomponent till alla våra route-komponenter) samt route
 //importerar våra komponenter/andra sidor
 import Navigation from './components/Navigation'
-import CharacterListitem from './components/CharacterListitem'
+//import CharacterListitem from './components/CharacterListitem'
 import HomePage from './pages/HomePage'
-import SearchStarWars from './pages/SearchStarWars'
+//import SearchStarWars from './pages/SearchStarWars'
 import NotFound from './pages/NotFound'
 import CharactersPage from './pages/CharactersPage'
 import FilmsPage from './pages/FilmsPage'
@@ -22,7 +22,7 @@ const App = () => {
 			<Container className="py-3">
 				<Routes> {/* Omsluter route med routes, lägger en path med det ewlement som ska renderas när sökvägen är / */}
 					<Route path="/" element={<HomePage />} /> 
-					<Route path="/search" element={<SearchStarWars />} /> {/* lägger en path med det ewlement som ska renderas när sökvägen är /search */}
+				{/*	<Route path="/search" element={<SearchStarWars />} />  lägger en path med det ewlement som ska renderas när sökvägen är /search */}
 					<Route path="/characters" element={<CharactersPage />} /> {/* lägger en path med det ewlement som ska renderas när sökvägen är /characters */}
 					<Route path="/characters/:id" element={<CharactersPage />} /> {/*vill komma åt mer info när vi trycker på en person. Så vi skriver element ={} så vi renderar CharactersPage vars uppgift ska vara att hämta en enskild person*/}
 					<Route path="/films" element={<FilmsPage />} />
