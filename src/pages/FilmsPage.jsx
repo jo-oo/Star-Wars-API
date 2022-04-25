bbddd007b7f2e4d434ce5775b016eeaac1a08847
jo-oo <johannakoreo@gmail.com>
2022-04-25 18:51:13 +0200

    Outprints episodes, release date and button to "more info"

--- src/pages/FilmsPage.jsx
+++ src/pages/FilmsPage.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from 'react'
-//import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import StarWarsAPI from '../services/StarWarsAPI'
-import { Card, ListGroup, Row, Col } from 'react-bootstrap'
+import ListGroup from 'react-bootstrap/listgroup'
+import { Card, Row, Col } from 'react-bootstrap'
+import Button from 'react-bootstrap/Button'
 
 
 const FilmsPage = () => {
@@ -24,19 +26,28 @@ const FilmsPage = () => {
 
 
 	//Here is what we ouput on our page
-	return (
-		<> 
-			<h1>Films</h1>
-		
-			{films && films.results.map((films) => ( //mappar över films-array. finns det charactaers så skriver den ut följande
-				<Card className="card">
-					<Card.Header>{films.title}</Card.Header> {/*outprints characters name */}
+  return (
+    <>
+    	<Row xs={1} md={3} className="g-4">
 
-				</Card>
-			))}
-	
-		</> 
-	)
+      	<h1>Films</h1>
+
+      	{films && films.results.map((films) => ( //mappar över films-array. finns det charactaers så skriver den ut följande
+        	<Col>
+          		<Card className="card">
+           			<Card.Header as="h5">{films.title}</Card.Header>
+          				<Card.Body>
+            				<Card.Text>Episode {films.episode_id} </Card.Text>
+            				<Card.Text>Released {films.release_date} </Card.Text>
+              				<Button variant="primary" as={Link} to={`/films/${films.episode_id}`}>More</Button>
+           				</Card.Body>
+            		</Card>
+        	</Col>
+        	))}
+        </Row>
+    </>
+  );
 }
+
 
 export default FilmsPage
