31536a9898e4169aa65b971c70a3acaa7482af44
jo-oo <johannakoreo@gmail.com>
2022-04-29 18:33:05 +0200

    Designed homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,24 @@
 const HomePage = () => {
 	return (
 		<>
-			<h2>✨ 🌟  ✨  🌟  ✨ 🌟  ✨ 🌟 ✨ 🌟  ✨ 🌟 ✨ 🌟  ✨ 🌟 ✨ 🌟  ✨ 🌟 ✨</h2>
-
-			<h1> ⭐ Welcome to a galaxy very close close away! ⭐ </h1>
-
-			<h2>✨ 🌟  ✨  🌟  ✨ 🌟  ✨ 🌟 ✨ 🌟  ✨ 🌟 ✨ 🌟  ✨ 🌟 ✨ 🌟  ✨ 🌟 ✨</h2>
-
+			<div className="mb-5 pb-5"
+				style={{display: 'flex', justifyContent:'center', alignItems:'center', height: '100vh'}}>
+				<div className="mb-5 pb-5 d-flex justify-content-around">
+					<div className="p-2 text-center">		
+						<h2>✨ 🌟  ✨  🌟  ✨ 🌟  ✨ 🌟 ✨ 🌟  ✨ 🌟 ✨ 🌟 ✨</h2>
+					</div>
+				</div>
+				<div className="mb-5 pb-5 d-flex justify-content-around">
+					<div className="p-2 text-center">		
+						<h2>⭐ Welcome to a galaxy very close close away! ⭐</h2>
+					</div>
+				</div>
+				<div className="mb-5 pb-5 d-flex justify-content-around">
+					<div className="p-2 text-center">		
+						<h2>✨ 🌟  ✨  🌟  ✨ 🌟  ✨ 🌟 ✨ 🌟  ✨ 🌟 ✨ 🌟 ✨</h2>
+					</div>
+				</div>
+			</div>
 		</>
 	)
 }
