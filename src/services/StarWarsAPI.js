0889d19df1b080ccdfbdfb0b0aa78c84dbe50aa9
jo-oo <johannakoreo@gmail.com>
2022-05-01 14:05:22 +0200

    removed unused API-methods

--- src/services/StarWarsAPI.js
+++ src/services/StarWarsAPI.js
@@ -9,46 +9,34 @@ import axios from 'axios'
 //axios.defaults.baseURL = 'https://swapi.dev/api'
 const BaseURL = 'https://swapi.dev/api'
 
-//Get all films
-const getFilms = async () => {
-		const response = await axios.get(`${BaseURL}/films`)
-		return response.data
-}
 
-//Get single film
-const getFilm = async (id) => {
-    const response = await axios.get(`${BaseURL}/films/${id}`)
-    return response.data
-}
-
-//get characters from API
-const getCharacters = async () => {
-		const response = await axios.get(`${BaseURL}/people`)
-		return response.data
-}
-
-//Get single character
-const getCharacter = async (id) => {
-    const response = await axios.get(`${BaseURL}/people/${id}`)
-    return response.data
-}
-
-//get new page of characters from API. The page number we send in is a variable
-const getCharactersPage = async (page) => {
-    const response = await axios.get(`${BaseURL}/people/?page=${page}`)
-    return response.data
-}
-
-//get new page of films from API. The page number we send in is a variable
+//Get new page of films from API. Only loads the single page we requested.
 const getFilmsPage = async (page) => {
     const response = await axios.get(`${BaseURL}/films/?page=${page}`)
     return response.data
 }
 
+//Get single film by id
+const getFilm = async (id) => {
+    const response = await axios.get(`${BaseURL}/films/${id}`)
+    return response.data
+}
+
+//Get new page of characters from API. Only loads the single page we requested.
+const getCharactersPage = async (page) => {
+    const response = await axios.get(`${BaseURL}/people/?page=${page}`)
+    return response.data
+}
+
+//Get single character by id
+const getCharacter = async (id) => {
+    const response = await axios.get(`${BaseURL}/people/${id}`)
+    return response.data
+}
+
+
 const exportedObject = {
-	getFilms,
     getFilm,
-	getCharacters,
     getCharacter,
     getCharactersPage,
     getFilmsPage,
